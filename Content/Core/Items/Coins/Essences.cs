d5f35fde35c7d48e7cc84b20eccfbf8630b9f75c
N0aW <you@example.com>
2024-06-20 01:59:56 +0300

    Added new Essence Sprites so they can be differentiated from souls, and don't bug out in the shop savings screen

--- Content/Core/Items/Coins/Essences.cs
+++ Content/Core/Items/Coins/Essences.cs
@@ -13,11 +13,9 @@ namespace TLR.Content.Core.Items.Coins
 	public class ForestEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/ForestEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -29,17 +27,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.LightGreen;
 		}
 	}
 	public class SnowEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/SnowEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -51,17 +46,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.LightBlue;
 		}
 	}
 	public class DesertEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/DesertEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -73,17 +65,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.LightYellow;
 		}
 	}
 	public class CorruptEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/CorruptEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -95,17 +84,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.Purple;
 		}
 	}
 	public class CrimsonEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/CrimsonEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -117,17 +103,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.Red;
 		}
 	}
 	public class DungeonEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/DungeonEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -139,17 +122,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.LightGray;
 		}
 	}
 	public class DungeonEssence2 : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/DungeonEssence2";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -161,17 +141,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.Gray;
 		}
 	}
 	public class JungleEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/JungleEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -183,17 +160,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.Green;
 		}
 	}
 	public class OceanEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/OceanEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -205,17 +179,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.SkyBlue;
 		}
 	}
 	public class GlowshroomEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/GlowshroomEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -227,17 +198,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.Blue;
 		}
 	}
 	public class HallowEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/HallowEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -249,17 +217,14 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.LightYellow;
 		}
 	}
 	public class UnderworldEssence : ModItem
 	{
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TLR.hjson' file.
-        public override string Texture => "TLR/Content/Core/Items/Coins/AnyEssence";
+        public override string Texture => "TLR/Content/Core/Items/Coins/EssenceSprites/UnderworldEssence";
 		public override void SetStaticDefaults() {
 			// Registers a vertical animation with 4 frames and each one will last 5 ticks (1/12 second)
-			Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(5, 4));
-			ItemID.Sets.AnimatesAsSoul[Item.type] = true; // Makes the item have an animation while in world (not held.). Use in combination with RegisterItemAnimation
 			ItemID.Sets.ItemIconPulse[Item.type] = true; // The item pulses while in the player's inventory
 			ItemID.Sets.ItemNoGravity[Item.type] = true; // Makes the item have no gravity
 			Item.ResearchUnlockCount = 25; // Configure the amount of this item that's needed to research it in Journey mode.
@@ -271,7 +236,6 @@ namespace TLR.Content.Core.Items.Coins
 			Item.value = Item.sellPrice(copper: 1);
 			Item.rare = ModContent.GetInstance<Rarities.Essence>().Type;
 			Item.maxStack = 9999;
-			Item.color = Color.OrangeRed;
 		}
 	}
 }
